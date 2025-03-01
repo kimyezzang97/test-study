b465e9512dc2798b02655e9149ce4307654f822d
kimyezzang97 <kimyezzang97@gmail.com>
2025-03-01 10:38:08 +0900

    테스트하기 어려운 영역을 분리하기

--- src/main/java/com/test_study/test_study/unit/CafeKiosk.java
+++ src/main/java/com/test_study/test_study/unit/CafeKiosk.java
@@ -4,11 +4,15 @@ import lombok.Getter;
 import org.aspectj.weaver.ast.Or;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 public class CafeKiosk {
+
+    public static final LocalTime SHOP_OPEN_TIME = LocalTime.of(10, 0);
+    public static final LocalTime SHOP_CLOSE_TIME = LocalTime.of(22, 0);
 
     private final List<Beverage> beverages = new ArrayList<>();
 
@@ -42,6 +46,23 @@ public class CafeKiosk {
     }
 
     public Order createOrder(){
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        LocalTime currentTime = currentDateTime.toLocalTime();
+
+        if(currentTime.isBefore(SHOP_OPEN_TIME) || currentTime.isAfter(SHOP_CLOSE_TIME)){
+            throw new IllegalArgumentException("주문 시간이 아닙니다. 관리자에게 문의하세요.");
+        }
+
+        return new Order(LocalDateTime.now(), beverages);
+    }
+
+    public Order createOrder(LocalDateTime currentDateTime){
+        LocalTime currentTime = currentDateTime.toLocalTime();
+
+        if(currentTime.isBefore(SHOP_OPEN_TIME) || currentTime.isAfter(SHOP_CLOSE_TIME)){
+            throw new IllegalArgumentException("주문 시간이 아닙니다. 관리자에게 문의하세요.");
+        }
+
         return new Order(LocalDateTime.now(), beverages);
     }
 }
