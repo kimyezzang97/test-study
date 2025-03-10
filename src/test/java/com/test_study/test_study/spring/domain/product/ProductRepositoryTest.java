94810b65e12c9f621fbac4daa96f43185a7a007b
kimyezzang97 <kimyezzang97@gmail.com>
2025-03-10 22:12:19 +0900

    persistence Layer 테스트2

---
+++ src/test/java/com/test_study/test_study/spring/domain/product/ProductRepositoryTest.java
@@ -0,0 +1,68 @@
+package com.test_study.test_study.spring.domain.product;
+
+import org.assertj.core.api.Assertions;
+import org.assertj.core.groups.Tuple;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static com.test_study.test_study.spring.domain.product.ProductSellingStatus.HOLD;
+import static com.test_study.test_study.spring.domain.product.ProductSellingStatus.SELLING;
+import static org.junit.jupiter.api.Assertions.*;
+
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // 기존 DB 설정 유지
+//@SpringBootTest
+@ActiveProfiles("test")
+@DataJpaTest
+class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @DisplayName("원하는 판매상태를 가진 상품들을 조회한다.")
+    @Test
+    void findAllBySellingStatusIn(){
+        //given
+        Product product1 = Product.builder()
+                .productNumber("001")
+                .type(ProductType.HANDMADE)
+                .sellingStatus(SELLING)
+                .name("아메리카노")
+                .price(4000)
+                .build();
+        Product product2 = Product.builder()
+                .productNumber("002")
+                .type(ProductType.HANDMADE)
+                .sellingStatus(HOLD)
+                .name("카페라떼")
+                .price(4500)
+                .build();
+        Product product3 = Product.builder()
+                .productNumber("003")
+                .type(ProductType.HANDMADE)
+                .sellingStatus(ProductSellingStatus.STOP_SELLING)
+                .name("팥빙수")
+                .price(7000)
+                .build();
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        //when
+        List<Product> products = productRepository.findAllBySellingStatusIn(List.of(SELLING, HOLD));
+
+        //then
+        Assertions.assertThat(products).hasSize(2)
+                .extracting("productNumber", "name","sellingStatus")
+                .containsExactlyInAnyOrder(
+                  Tuple.tuple("001", "아메리카노", SELLING),
+                        Tuple.tuple("002", "카페라떼", HOLD)
+                );
+    }
+
+
+}
